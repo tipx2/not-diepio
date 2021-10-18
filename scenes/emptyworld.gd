2c121a9e272585fd487343aeb2b579465982bc4e
unknown <aryehtys@gmail.com>
2021-10-18 19:50:08 +0100

    fixed player banner on winner

--- scenes/emptyworld.gd
+++ scenes/emptyworld.gd
@@ -147,13 +147,10 @@ func handle_death1():
 			var camera = get_node('Camera2D')
 			var middle = camera.get_camera_screen_center()
 			get_node('winAvatar').texture = load("res://Large Portraits/" + big_shape_name + "-big.png")
-#fix winbanner
-#			get_node("winBanner").texture = load("res://assets/player1banner.png")
 			get_node("winBackground").position = middle
 			get_node("winAvatar").position = middle
 			get_node("winForeground").position = middle
 			get_node("winForeground").position.y += 200 * camera.zoom.y
-#			get_node("winBanner").position = middle
 			var scalex = camera.get_viewport().size.x / get_node("winBackground").texture.get_size().x
 			var scaley = camera.get_viewport().size.y / get_node("winBackground").texture.get_size().y
 			get_node("winBackground").scale = Vector2(scalex, scaley) * camera.zoom
@@ -162,8 +159,11 @@ func handle_death1():
 			get_node("winAvatar").visible = true
 			get_node("winForeground").scale = 15 * camera.zoom
 			get_node("winForeground").visible = true
-#			get_node("winBanner").scale = 15 * camera.zoom
-#			get_node("winBanner").visible = true
+			get_node("winBanner").texture = load("res://assets/player1banner.png")
+			get_node("winBanner").scale = Vector2(4, 2) * camera.zoom
+			get_node("winBanner").visible = true
+			get_node("winBanner").position = middle
+			get_node("winBanner").position.y -= 350 * camera.zoom.y
 			t.set_wait_time(5)
 			t.start()
 			yield(t, "timeout")
@@ -233,6 +233,11 @@ func handle_death2():
 			get_node("winAvatar").visible = true
 			get_node("winForeground").scale = 15 * camera.zoom
 			get_node("winForeground").visible = true
+			get_node("winBanner").texture = load("res://assets/player2banner.png")
+			get_node("winBanner").scale = Vector2(4, 2) * camera.zoom
+			get_node("winBanner").visible = true
+			get_node("winBanner").position = middle
+			get_node("winBanner").position.y -= 350 * camera.zoom.y
 			t.set_wait_time(5)
 			t.start()
 			yield(t, "timeout")
